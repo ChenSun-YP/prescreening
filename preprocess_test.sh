e6bd3292dfea22e8dc96833e0235825300090bd7
Ben Ren <benren@discovery1.hpc.usc.edu>
2026-02-24 16:37:38 -0800

    run with all levels of coarseness

--- preprocess_test.sh
+++ preprocess_test.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=preprocess_test
 #SBATCH --output=sbatch_outputs/preprocess_test_%j.out
 #SBATCH --error=sbatch_outputs/preprocess_test_%j.err
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=1
 
