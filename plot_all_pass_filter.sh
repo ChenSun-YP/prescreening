02fba8a19a9c43c770e77acb11154498f0c521e3
BenR <90284651+AsianUser@users.noreply.github.com>
2026-05-05 00:56:07 -0700

    plots all the cross corr that pass the filters

---
+++ plot_all_pass_filter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=plot_all_pass_filter
+#SBATCH --output=sbatch_outputs/plot_all_pass_filter_%j.out
+#SBATCH --error=sbatch_outputs/plot_all_pass_filter_%j.err
+#SBATCH --time=1:00:00
+#SBATCH --mem=4G
+#SBATCH --cpus-per-task=1
+
+module purge
+module load conda
+source /apps/conda/miniforge3/24.11.3/etc/profile.d/conda.sh
+
+conda activate plasticityvenv
+
+python -u plot_good_pairs.py --out /project2/dsong_945/BenR/prescreening/1029_good_pairs_ccg.png
+
+conda deactivate
